c2236b144e2e0a8a7846731ef194239d1fef4623
Fábio "DannyIsYog" Sousa <fabio.sousa@tecnico.ulisboa.pt>
2022-12-09 21:09:36 +0000

    Cleanup map generation

--- Assets/Scripts/Map/MapManager.cs
+++ Assets/Scripts/Map/MapManager.cs
@@ -11,10 +11,14 @@ public class MapManager : MonoBehaviour
     // Start is called before the first frame update
 
     public List<GameObject> avalibleCollectibles = new List<GameObject>();
+
+    // chance to spawn collectibles in section serialized in inspector
+    [Range(0, 100)]
+    public int chanceToSpawnCollectible = 50;
     void Start()
     {
-        GenerateMap();
-        // call move sections every second
+        // generate first three sections
+        GenerateSection(3);
     }
 
     // Update is called once per frame
@@ -25,48 +29,44 @@ public class MapManager : MonoBehaviour
         // if we only have three section left, generate new section
         if (sections.Count < 3)
         {
-            GenerateSection();
+            GenerateSection(1);
         }
 
     }
 
-    public void GenerateMap()
+    // generate one section
+    public void GenerateSection(int quantity)
     {
-        for (int i = 0; i < 2; i++)
+        for (int i = 0; i < quantity; i++)
         {
+            // get random section from avalibleSections list
             int randomIndex = Random.Range(0, avalibleSections.Count);
+            // instantiate section
             GameObject newSection = Instantiate(avalibleSections[randomIndex], transform);
             //get lenght of section
             float sectionLength = newSection.transform.localScale.x;
-            newSection.transform.position = new Vector3(sectionLength * i, 0, 0);
-            sections.Add(newSection);
-
-            // Get CollectiblesSpawnPoints chilren from section
-            Transform[] collectiblesSpawnPoints = newSection.GetComponentsInChildren<Transform>();
-            // loop through all children and randomly spawn collectibles with 50% chance
+            // set position of new section next to the last section on the list, if there is no sections on the list, set position to 0
+            if (sections.Count > 0)
+                newSection.transform.position = new Vector3(sections[sections.Count - 1].transform.position.x + sectionLength, 0, 0);
+            else
+                newSection.transform.position = new Vector3(0, 0, 0);
+            // Get children of new section with tag "SpawnPoint"
+            var collectiblesSpawnPoints = GetComponentsInChildren(newSection.transform, "SpawnPoint");
+            // loop through all children and randomly spawn collectibles with chance = chanceToSpawnCollectible
             foreach (Transform collectiblesSpawnPoint in collectiblesSpawnPoints)
             {
                 int randomCollectibleIndex = Random.Range(0, avalibleCollectibles.Count);
-                int randomChance = Random.Range(0, 10);
-                if (randomChance == 0)
+                int randomChance = Random.Range(0, 100);
+                if (randomChance < chanceToSpawnCollectible)
                 {
                     GameObject newCollectible = Instantiate(avalibleCollectibles[randomCollectibleIndex], collectiblesSpawnPoint.position, Quaternion.identity);
                     newCollectible.transform.parent = collectiblesSpawnPoint;
                 }
             }
+            // add new section to sections list
+            sections.Add(newSection);
         }
-    }
 
-    // generate one section
-    public void GenerateSection()
-    {
-        int randomIndex = Random.Range(0, avalibleSections.Count);
-        GameObject newSection = Instantiate(avalibleSections[randomIndex], transform);
-        //get lenght of section
-        float sectionLength = newSection.transform.localScale.x;
-        // set position of new section next to the last section on the list
-        newSection.transform.position = new Vector3(sections[sections.Count - 1].transform.position.x + sectionLength, 0, 0);
-        sections.Add(newSection);
     }
 
     // check if the first section is out of camera view (the camera moves with the player) and destroy it
@@ -86,6 +86,20 @@ public class MapManager : MonoBehaviour
             Destroy(sections[0]);
             sections.RemoveAt(0);
         }
+    }
+
+    // return all children with tag
+    public Transform[] GetComponentsInChildren(Transform parent, string tag)
+    {
+        List<Transform> childrenWithTag = new List<Transform>();
+        foreach (Transform child in parent)
+        {
+            if (child.tag == tag)
+            {
+                childrenWithTag.Add(child);
+            }
+        }
+        return childrenWithTag.ToArray();
     }
 
 

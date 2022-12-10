b3c36a3ce36daa77a3ef57f333f425e390012172
Fábio "DannyIsYog" Sousa <fabio.sousa@tecnico.ulisboa.pt>
2022-12-10 23:27:46 +0000

    Another one

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Transactions;
 using TMPro;
-using UnityEditor.PackageManager.Requests;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
